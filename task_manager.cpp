e729dfcc9a7adfbd721e95a2ac273dcd101c1f5a
Eryk Kopka <eryk.kopka1@gmail.com>
2026-02-11 20:31:09 +0100

    Add menu

---
+++ task_manager.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){  //{}  =
+    int menu;
+    vector<string> zadania;
+
+    do{
+        cout<<"1 - Dodaj zadanie"<<endl;
+        cout<<"2 - Usun zadanie"<<endl;
+        cout<<"0 - Wyjscie"<<endl;
+
+        cin>>menu;
+
+        switch(menu){
+            case 1:{
+
+                break;
+            }
+            case 2:{
+                break;
+            }
+        }
+
+    }while(menu!=0);
+    return 0;
+}
