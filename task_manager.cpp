982bcb620081628afcb48e9321c407c2b188283f
Eryk Kopka <eryk.kopka1@gmail.com>
2026-02-11 20:45:27 +0100

    Add task adding

--- task_manager.cpp
+++ task_manager.cpp
@@ -2,23 +2,31 @@
 #include<vector>
 using namespace std;
 
-int main(){  //{}  =
+int main(){  //{}  =  +  g++ task_manager.cpp -o task_manager.exe 
     int menu;
     vector<string> zadania;
 
+
     do{
-        cout<<"1 - Dodaj zadanie"<<endl;
-        cout<<"2 - Usun zadanie"<<endl;
+        cout<<"1 - Wyswietl zadania"<<endl;
+        cout<<"2 - Dodaj zadanie"<<endl;
+        cout<<"3 - Usun zadanie"<<endl;
         cout<<"0 - Wyjscie"<<endl;
 
         cin>>menu;
 
         switch(menu){
             case 1:{
-
+                
                 break;
             }
             case 2:{
+                break;
+            }
+            case 3:{
+                zadania.resize(zadania.size()+1);
+                cout<<"Wpisz tresc zadania: ";
+                cin>>zadania.back();
                 break;
             }
         }
