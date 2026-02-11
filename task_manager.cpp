ffc9a31deb96285c88a8921fa3f3cb5b2168f155
Eryk Kopka <eryk.kopka1@gmail.com>
2026-02-11 21:52:32 +0100

    Implement: Tasks always shown
    terminal gets cleared after every action and tasks are always shown at the top with options below

--- task_manager.cpp
+++ task_manager.cpp
@@ -3,6 +3,10 @@
 #include<fstream>
 using namespace std;
 
+void clearScreen(){
+    cout<<"\033[2J\033[1;1H";
+}
+
 int main(){  //{}  =  +  g++ task_manager.cpp -o task_manager.exe
     int menu;
     int tmp;
@@ -17,27 +21,26 @@ int main(){  //{}  =  +  g++ task_manager.cpp -o task_manager.exe
 
 
     do{
-        cout<<"1 - Wyswietl zadania"<<endl;
-        cout<<"2 - Dodaj zadanie"<<endl;
-        cout<<"3 - Usun zadanie"<<endl;
+        clearScreen();
+        for(int i=0;i<zadania.size();i++){
+            cout<<i+1<<" "<<zadania.at(i)<<endl;
+        }
+        cout<<endl<<endl;
+
+        cout<<"1 - Dodaj zadanie"<<endl;
+        cout<<"2 - Usun zadanie"<<endl;
         cout<<"0 - Wyjscie"<<endl;
 
         cin>>menu;
 
         switch(menu){
             case 1:{
-                for(int i=0;i<zadania.size();i++){
-                    cout<<i+1<<" "<<zadania.at(i)<<endl;
-                }
-                break;
-            }
-            case 2:{
                 zadania.resize(zadania.size()+1);
                 cout<<"Wpisz tresc zadania: ";
                 cin>>zadania.back();
                 break;
             }
-            case 3:{
+            case 2:{
                 cout<<"Ktore zadanie usunac: ";
                 cin>>tmp;
                 if(zadania.size()>=tmp and tmp>0){
